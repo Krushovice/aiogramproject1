b03df67802dc1f55aa3f948ba4edabc8f6167533
Krushovice <kickstar69@yandex.ru>
2024-06-02 15:38:56 +1000

    create session middleware

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,7 +1,33 @@
 __all__ = (
     "settings",
     "db_helper",
+    "Base",
+    "User",
+    "Book",
+    "Author",
+    "UserRead",
+    "BookRead",
+    "AuthorRead",
+    "UserUpdatePartial",
+    "BookUpdatePartial",
+    "AuthorUpdatePartial",
+    "DataBaseSession",
 )
 
 from .config import settings
-from core.models.db_helper import db_helper
+from .models.db_helper import db_helper
+from .models import User, Book, Author, Base
+from .middlewares.session_middleware import DataBaseSession
+
+from .schemas.user_schemas import (
+    UserRead,
+    UserUpdatePartial,
+)
+from .schemas.book_schemas import (
+    BookRead,
+    BookUpdatePartial,
+)
+from .schemas.author_schemas import (
+    AuthorRead,
+    AuthorUpdatePartial,
+)
